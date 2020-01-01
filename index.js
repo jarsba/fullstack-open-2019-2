74f7da0d99466dae500f79c980854fb25b8ab99f
jarsba <jarkko.lehtiranta@selko.io>
2020-01-01 19:05:58 +0200

    backend init

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+
+app.use(bodyParser.json())
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) });
+app.use(morgan(':method :url :status :req[content-length] - :response-time ms :body'));
+
+const cors = require('cors')
+
+app.use(cors())
+
+let persons = require("./db.json");
+
+const generateId = () => {
+    return Math.floor(Math.random() * 1000000)
+}
+
+app.get('/', (request, response) => {
+    response.send('Hello World!')
+})
+
+app.get('/info', (request, response) => {
+    response.send(`Phonebook has info for ${persons.length} people \n\n${new Date(Date.now())}`)
+    response.end()
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'name or number missing'
+        })
+    }
+
+    if(persons.filter(person => person.name === body.name).length > 0 ) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
+
+    persons = persons.concat(person)
+
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (!person) {
+        return response.status(404).json({
+            error: 'person not found'
+        })
+    }
+
+    response.json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    response.status(204).end()
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
