b7fe5e0f892e3c1497a25241f728ac14524ce8cc
jarsba <jarkko.lehtiranta@selko.io>
2020-01-01 19:20:12 +0200

    uibuild

---
+++ build/precache-manifest.dcc00c58f5c89f64c409884449327573.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "623938333386f61424c815e0d492d387",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f954e218029a4d763a9d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "e5f3ebe8e0a8172b7800",
+    "url": "/static/js/2.e5433f4a.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.e5433f4a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f954e218029a4d763a9d",
+    "url": "/static/js/main.64bf6ef0.chunk.js"
+  },
+  {
+    "revision": "57d8af95778078760a0e",
+    "url": "/static/js/runtime-main.283795fd.js"
+  }
+]);
